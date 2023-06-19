ad16fc508ceb06452b9518800caf31eb911d45f0
Kanda Mikiko <kmikiko1121@gmail.com>
2023-06-19 22:14:22 +0900

    step3 to step4

--- vm/vending_machine.rb
+++ vm/vending_machine.rb
@@ -10,11 +10,18 @@ class VendingMachine
     # 最初の自動販売機に入っている金額は0円
     @slot_money = 0
     @cola = {name: "コーラ", price: 150, stock: 5 }
-    @water = {name: "水", price: 150, stock: 5 }
-    @tea = {name: "茶",price: 150, stock: 5 }
+    @water = {name: "水", price: 100, stock: 5 }
+    @red_bull = {name: "レッドブル",price: 200, stock: 5 }
     @sales = 0
-    @drinks = [@cola, @water, @tea]
+    @drinks = [@cola, @water, @red_bull]
   end
+
+  def inform
+    p @drinks.map do |drink|
+      drink[:price] < @slot_money && drink[:stock] => 1
+  end
+
+end
 
   # 投入金額の総計を取得できる。
   def current_slot_money
@@ -40,16 +47,45 @@ class VendingMachine
     @slot_money = 0
   end
 
-  def kounyu
-    if @cola[:stock] == 0 
-      puts "売り切れ"
-    elsif  @cola[:price] > @slot_money
-      puts "お金を入れて"
-    else 
-      @cola[:stock] -= 1
-      @sales += @cola[:price]
-      @slot_money -= @cola[:price]
-    end
+  def sell(drinks)
+    case drinks
+    when @cola
+    
+      if @cola[:stock] == 0 
+        puts "売り切れ"
+      elsif  @cola[:price] > @slot_money
+        puts "お金を入れて"
+      else 
+        @cola[:stock] -= 1
+        @sales += @cola[:price]
+        @slot_money -= @cola[:price]
+      end
+    
+    when @water
+
+      if @water[:stock] == 0 
+        puts "売り切れ"
+      elsif  @water[:price] > @slot_money
+        puts "お金を入れて"
+      else 
+        @water[:stock] -= 1
+        @sales += @water[:price]
+        @slot_money -= @water[:price]
+      end
+
+    when @red_bull
+
+      if @red_bull[:stock] == 0 
+        puts "売り切れ"
+      elsif  @red_bull[:price] > @slot_money
+        puts "お金を入れて"
+      else 
+        @red_bull[:stock] -= 1
+        @sales += @red_bull[:price]
+        @slot_money -= @red_bull[:price]
+      end
+
+
     # puts @cola[:stock]
     # puts @sales
     # puts @slot_money
@@ -58,4 +94,25 @@ class VendingMachine
   def test
     puts @drinks[0]
   end
+
+  
+  
+  def replenished(drink_name,drink_price,drink_stock)
+    drink_name = {}
+    drink_name [price] = drink_price
+    drink_name [stock] = drink_stock
+    drinks = {}
+    drinks [drink_nmae] = drink_
+    #drink_types = {name: drink_name, price: drink_price, stock: drink_stock}
+    @drinks = { drink_name: {price: drink_price, stock: drink_stock}}
+    #drinks << drink_types
+    @cola = {price: 150, stock: 5 }
+    @water = {price: 150, stock: 5 }
+    @tea = {price: 150, stock: 5 }  
+  end
+
+
+  def 
 end
+
+# http://devtesting.jp/tddbc/?TDDBC%E5%A4%A7%E9%98%AA3.0%2F%E8%AA%B2%E9%A1%8C
